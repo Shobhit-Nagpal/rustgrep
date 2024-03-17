5b7c61ebb33bd0eb222c1ca5e08c0ba3f5ac9fb3
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-03-17 21:50:25 +0530

    Basic version up

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::process;
+
+use rustgrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem passing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = rustgrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
+
